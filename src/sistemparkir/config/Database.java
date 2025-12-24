1f5006f294d3d4d365aca33ba66bea6f8923cf8c
Adrian <Adrian@LAPTOP-34SKT42F>
2025-12-24 14:15:36 +0700

    gw baru tambahin folder database, model

---
+++ src/sistemparkir/config/Database.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemparkir.config;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Adrian
+ */
+public class Database {
+    
+}
--- src/sistemparkir/view/DaftarParkir.java
+++ src/sistemparkir/view/DaftarParkir.java
@@ -92,6 +92,7 @@ public class DaftarParkir extends javax.swing.JPanel {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
 
